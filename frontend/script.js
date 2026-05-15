774e1bfaf9457dc0522909a039c2d675b0857777
Shahajadbi K Mokashi <278966384+shahzadbimokashi-rgb@users.noreply.github.com>
2026-05-15 16:52:23 +0530

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -1,1 +1,19 @@
+function send() {
+    let msg = document.getElementById("msg").value;
 
+    if (!msg) return;
+
+    document.getElementById("chatbox").innerHTML +=
+        "<p><b>You:</b> " + msg + "</p>";
+
+    fetch("http://localhost:5000/chat", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ message: msg })
+    })
+    .then(res => res.json())
+    .then(data => {
+        document.getElementById("chatbox").innerHTML +=
+            "<p><b>AI:</b> " + data.reply + "</p>";
+    });
+}
